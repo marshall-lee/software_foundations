090bce2aa3b8a58a0756ecf1e7361a2734548b5d
Vladimir Kochnev <hashtable@yandex.ru>
2017-02-28 21:41:52 +0400

    Initial commit.

---
+++ Bib.v
@@ -0,0 +1,61 @@
+(** * Bib: Bibliography *)
+
+(**
+
+[Appel 2017] Andrew Appel.  Verified Functional Algorithms (volume
+  3 in the Software Foundations series).
+  https://www.cs.princeton.edu/~appel/vfa/
+
+[Aydemir 2008] Brian Aydemir, Arthur Charguéraud, Benjamin
+  C. Pierce, Randy Pollack, and Stephanie Weirich. Engineering Formal
+  Metatheory. In ACM SIGPLAN-SIGACT Symposium on Principles of
+  Programming Languages (POPL), San Francisco, California, pages
+  3-15. ACM, January 2008.
+  http://www.cis.upenn.edu/~bcpierce/papers/binders.pdf
+
+[Bertot 2004] Interactive Theorem Proving and Program Development:
+  Coq'Art: The Calculus of Inductive Constructions, by Yves Bertot and
+  Pierre Casteran.  Springer-Verlag, 2004.
+  http://tinyurl.com/z3o7nqu
+
+[Chlipala 2013] Certified Programming with Dependent Types, by
+  Adam Chlipala.  MIT Press.  2013.  http://tinyurl.com/zqdnyg2
+
+[Harper 2015] Practical Foundations for Programming Languages, by
+  Robert Harper.  Cambridge University Press.  Second edition, 2016.
+  http://tinyurl.com/z82xwta
+
+[Lipovaca 2011] Learn You a Haskell for Great Good! A Beginner's
+  Guide, by Miran Lipovaca, No Starch Press, April 2011.
+  http://learnyouahaskell.com
+
+[Mitchell 1996] Foundations for Programming Languages, by John
+  C. Mitchell.  MIT Press, 1996.  http://tinyurl.com/zkosavw
+
+[Nipkow 2014] Concrete Semantics with Isabelle/HOL, by Tobias
+  Nipkow and Gerwin Klein.  Springer 2014.
+  http://www.concrete-semantics.org
+
+[Pierce 2002] Types and Programming Languages, by Benjamin
+  C. Pierce. MIT Press, 2002.  http://tinyurl.com/gtnudmu
+
+[O'Sullivan 2008] Bryan O'Sullivan, John Goerzen, and Don Stewart:
+  Real world Haskell - code you can believe in. O'Reilly
+  2008. http://book.realworldhaskell.org
+
+[Pugh 1991] Pugh, William. "The Omega test: a fast and practical
+  integer programming algorithm for dependence analysis." Proceedings
+  of the 1991 ACM/IEEE conference on Supercomputing. ACM, 1991.
+  http://dl.acm.org/citation.cfm?id=125848
+
+[Wadler 2015] Philip Wadler. "Propositions as types."
+  Communications of the ACM 58, no. 12 (2015): 75-84.
+  http://dl.acm.org/citation.cfm?id=2699407
+
+[Winskel 1993] The Formal Semantics of Programming Languages: An
+  Introduction, by Glynn Winskel.  MIT Press, 1993.
+  http://tinyurl.com/j2k6ev7
+
+*)
+
+(* $Date: 2016-12-08 14:15:18 -0500 (Thu, 08 Dec 2016) $ *)
